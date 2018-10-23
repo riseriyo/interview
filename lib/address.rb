cc0a708d8720fd8cda6a7c06b92ac67795412b32
rriyo <rriyo@go2ris.com>
2018-10-22 21:53:13 -0700

    Render a table coordinates

--- lib/address.rb
+++ lib/address.rb
@@ -1,5 +1,15 @@
 require_relative 'geocoding'
 
 class Address
-  attr_accessor :lat, :lng, :full_address
+
+  attr_accessor :latitude, :longitude, :full_address
+
+  # call geocode for address lookup
+  #reverse_geocoded_by :latitude, :longitude
+
+  #after_validation :reverse_geocode
+
+  def self.reverse_geocode
+    return :full_address
+  end
 end
--- main.rb
+++ main.rb
@@ -1,8 +1,26 @@
 require 'sinatra/base'
 Dir['./lib/*.rb'].each { |f| require f }
 
+#require 'yaml/store'
+
 class Main < Sinatra::Base
+
+
   get '/' do
     erb :index #, locals: { address: address }
   end
+
+  post '/addresses' do
+    # {coordinate => [address,distance]}
+    @v = []
+    @params = params
+
+    #params.keys.each do | k |
+      #puts k
+      #@result = Geocoder.reverse_geocode(@params[k])
+      #@v << @result
+    #end
+    erb :addresses
+  end
+
 end
